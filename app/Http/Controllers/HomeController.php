b6fd0d7dbcfe50f75e9bd9e6c3059f42626ebef2
vaghasiyaankur <ankurvaghasiya21@gmail.com>
2022-12-22 15:21:36 +0530

    notificartion check in live

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Customer;
+use Kutia\Larafirebase\Facades\Larafirebase;
+use Notification;
+use App\Notifications\SendPushNotification;
+
+class HomeController extends Controller
+{
+    public function updateToken(Request $request){
+        try{
+            // $request->user()->update(['fcm_token'=>$request->token]);
+            $request->session()->put('device_token',$request->token);
+            return response()->json([
+                'success'=>true
+            ]);
+        }catch(\Exception $e){
+            report($e);
+            return response()->json([
+                'success'=>false
+            ],500);
+        }
+    }
+
+    public function notification(Request $request){
+        // $request->validate([
+        //     'title'=>'required',
+        //     'message'=>'required'
+        // ]);
+    
+        try{
+            $fcmTokens = ['c16H1_gwueT8jzmm2w_cTn:APA91bGjH092huMhvCN4Cejb84y1Y_CxzdbLrxIwyLucbUCyX4v1gl2O6oYcVaSm0ncnYhD9mbFlKVmvAgVzeePzLN5yhn0PG1esfjo0P1mrR0RUXb_W4sQII_GfcZoXodUmsqc-Kg0m'];
+    
+            //Notification::send(null,new SendPushNotification($request->title,$request->message,$fcmTokens));
+    
+            /* or */
+    
+            //auth()->user()->notify(new SendPushNotification($title,$message,$fcmTokens));
+    
+            /* or */
+    
+            $test = Larafirebase::withTitle('test')
+                ->withBody('test')
+                ->sendMessage($fcmTokens);
+            // dd($test);
+            // return redirect()->back()->with('success','Notification Sent Successfully!!');
+    
+        }catch(\Exception $e){
+            report($e);
+            dd($e);
+            // return redirect()->back()->with('error','Something goes wrong while sending notification.');
+        }
+    }
+
+    public function notification1()
+    {
+        $fcmTokens = ['c16H1_gwueT8jzmm2w_cTn:APA91bGjH092huMhvCN4Cejb84y1Y_CxzdbLrxIwyLucbUCyX4v1gl2O6oYcVaSm0ncnYhD9mbFlKVmvAgVzeePzLN5yhn0PG1esfjo0P1mrR0RUXb_W4sQII_GfcZoXodUmsqc-Kg0m'];
+        // dd(new SendPushNotification('$title','$message',$fcmTokens));
+        dd(Notification::send(null,new SendPushNotification('$title','$message',$fcmTokens)));
+
+    }
+}
+
+
+?>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Food Menu</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
 	<!-- ==========FONTAWSOME CDN LINK ================== -->
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" /> --}}
     <!-- ============GOOGLE FONTS LINK================== -->
@@ -31,7 +33,91 @@
     </div>
 
 	@vite('resources/js/app.js')
-    <script src="https://www.gstatic.com/firebasejs/7.23.0/firebase.js"></script>
+
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+    https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+
+    
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBfphAIxpzsJDUuCCOhF6DtZKqUPxgj-wA",
+        authDomain: "fir-test-25564.firebaseapp.com",
+        projectId: "fir-test-25564",
+        storageBucket: "fir-test-25564.appspot.com",
+        messagingSenderId: "1064311492584",
+        appId: "1:1064311492584:web:4842b73ccef0b65aeade5d",
+        measurementId: "G-B3L0LYKB05"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    const messaging = firebase.messaging();
+
+    function initFirebaseMessagingRegistration() {
+        messaging.requestPermission().then(function () {
+            return messaging.getToken()
+        }).then(function(token) {
+            
+            const tokens = document.head.querySelector('meta[name="csrf-token"]');
+
+            const headers = {
+            'X-CSRF-TOKEN': tokens.content,
+            'Access-Control-Allow-Origin': '*',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/json',
+            };
+
+            // axios.post("{{ route('fcmToken') }}",{
+            //     _method:"PATCH",
+            //     token,
+            // }, headers).then(({data})=>{
+            //     console.log(data)
+            // }).catch(({response:{data}})=>{
+            //     console.error(data)
+            // })
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+
+            $.ajax({
+                url: '{{ route("fcmToken") }}',
+                type: 'POST',
+                data: {
+                    token: token
+                },
+                dataType: 'JSON',
+                success: function (response) {
+                    alert('Token saved successfully.');
+                },
+                error: function (err) {
+                    console.log('User Chat Token Error'+ err);
+                },
+            });
+
+        }).catch(function (err) {
+            console.log(`Token Error :: ${err}`);
+        });
+    }
+
+    initFirebaseMessagingRegistration();
+  
+    messaging.onMessage(function({data:{body,title}}){
+        new Notification(title, {body});
+    });
+</script>
+
 	<!--============ BOOTSTRAP JS LINK ===============-->
 
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/framework7/1.5.2/js/framework7.min.js"></script> --}}
