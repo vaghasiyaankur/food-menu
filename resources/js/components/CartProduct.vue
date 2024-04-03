a961d131da7cdde3452317ffc68c62c60e4f8ce0
vaghasiyaankur <ankur@juvo.ie>
2024-04-03 18:36:22 +0530

    add product in pos page

---
+++ resources/js/components/CartProduct.vue
@@ -0,0 +1,193 @@
+<template>
+    <div class="cart-products padding-half">
+        <div class="product_detail padding display-flex justify-content-space-between align-items-center">
+            <div class="delete-product">
+                <span><f7-icon f7="minus" class="font-16 delete-product-button"></f7-icon></span>
+            </div>
+            <div class="product-summary margin-horizontal-half">
+                <p class="no-margin margin-bottom-half">Margherita Pizza</p>
+                <p class="no-margin"><span>Size: S</span> <span class="text-red">$10.00</span></p>
+            </div>
+            <div class="quantity-section text-align-center padding">
+                <div class="quantity-content display-flex justify-content-center align-items-center">
+                    <span class="quantity-minus padding-vertical-half"><f7-icon f7="minus" class="font-16"></f7-icon></span>
+                    <span class="quantity-count padding-vertical-half">1</span>
+                    <span class="quantity-plus padding-vertical-half"><f7-icon f7="plus" class="font-16"></f7-icon></span>
+                </div>
+            </div>
+            <div class="product-note-content text-align-center">
+                <div class="product-note"><Icon name="note" /></div>
+            </div>
+        </div>
+        <div class="product_detail padding display-flex justify-content-space-between align-items-center">
+            <div class="delete-product">
+                <span><f7-icon f7="minus" class="font-16 delete-product-button"></f7-icon></span>
+            </div>
+            <div class="product-summary margin-horizontal-half">
+                <p class="no-margin margin-bottom-half">Margherita Pizza</p>
+                <p class="no-margin"><span>Size: S</span> <span class="text-red">$10.00</span></p>
+            </div>
+            <div class="quantity-section text-align-center padding">
+                <div class="quantity-content display-flex justify-content-center align-items-center">
+                    <span class="quantity-minus padding-vertical-half"><f7-icon f7="minus" class="font-16"></f7-icon></span>
+                    <span class="quantity-count padding-vertical-half">1</span>
+                    <span class="quantity-plus padding-vertical-half"><f7-icon f7="plus" class="font-16"></f7-icon></span>
+                </div>
+            </div>
+            <div class="product-note-content text-align-center">
+                <div class="product-note"><Icon name="note" /></div>
+            </div>
+        </div>
+        <div class="product_detail padding display-flex justify-content-space-between align-items-center">
+            <div class="delete-product">
+                <span><f7-icon f7="minus" class="font-16 delete-product-button"></f7-icon></span>
+            </div>
+            <div class="product-summary margin-horizontal-half">
+                <p class="no-margin margin-bottom-half">Margherita Pizza</p>
+                <p class="no-margin"><span>Size: S</span> <span class="text-red">$10.00</span></p>
+            </div>
+            <div class="quantity-section text-align-center padding">
+                <div class="quantity-content display-flex justify-content-center align-items-center">
+                    <span class="quantity-minus padding-vertical-half"><f7-icon f7="minus" class="font-16"></f7-icon></span>
+                    <span class="quantity-count padding-vertical-half">1</span>
+                    <span class="quantity-plus padding-vertical-half"><f7-icon f7="plus" class="font-16"></f7-icon></span>
+                </div>
+            </div>
+            <div class="product-note-content text-align-center">
+                <div class="product-note"><Icon name="note" /></div>
+            </div>
+        </div>
+        <div class="product_detail padding display-flex justify-content-space-between align-items-center">
+            <div class="delete-product">
+                <span><f7-icon f7="minus" class="font-16 delete-product-button"></f7-icon></span>
+            </div>
+            <div class="product-summary margin-horizontal-half">
+                <p class="no-margin margin-bottom-half">Margherita Pizza</p>
+                <p class="no-margin"><span>Size: S</span> <span class="text-red">$10.00</span></p>
+            </div>
+            <div class="quantity-section text-align-center padding">
+                <div class="quantity-content display-flex justify-content-center align-items-center">
+                    <span class="quantity-minus padding-vertical-half"><f7-icon f7="minus" class="font-16"></f7-icon></span>
+                    <span class="quantity-count padding-vertical-half">1</span>
+                    <span class="quantity-plus padding-vertical-half"><f7-icon f7="plus" class="font-16"></f7-icon></span>
+                </div>
+            </div>
+            <div class="product-note-content text-align-center">
+                <div class="product-note"><Icon name="note" /></div>
+            </div>
+        </div>
+        <div class="product_detail padding display-flex justify-content-space-between align-items-center">
+            <div class="delete-product">
+                <span><f7-icon f7="minus" class="font-16 delete-product-button"></f7-icon></span>
+            </div>
+            <div class="product-summary margin-horizontal-half">
+                <p class="no-margin margin-bottom-half">Margherita Pizza</p>
+                <p class="no-margin"><span>Size: S</span> <span class="text-red">$10.00</span></p>
+            </div>
+            <div class="quantity-section text-align-center padding">
+                <div class="quantity-content display-flex justify-content-center align-items-center">
+                    <span class="quantity-minus padding-vertical-half"><f7-icon f7="minus" class="font-16"></f7-icon></span>
+                    <span class="quantity-count padding-vertical-half">1</span>
+                    <span class="quantity-plus padding-vertical-half"><f7-icon f7="plus" class="font-16"></f7-icon></span>
+                </div>
+            </div>
+            <div class="product-note-content text-align-center">
+                <div class="product-note"><Icon name="note" /></div>
+            </div>
+        </div>
+        <div class="product_detail padding display-flex justify-content-space-between align-items-center">
+            <div class="delete-product">
+                <span><f7-icon f7="minus" class="font-16 delete-product-button"></f7-icon></span>
+            </div>
+            <div class="product-summary margin-horizontal-half">
+                <p class="no-margin margin-bottom-half">Margherita Pizza</p>
+                <p class="no-margin"><span>Size: S</span> <span class="text-red">$10.00</span></p>
+            </div>
+            <div class="quantity-section text-align-center padding">
+                <div class="quantity-content display-flex justify-content-center align-items-center">
+                    <span class="quantity-minus padding-vertical-half"><f7-icon f7="minus" class="font-16"></f7-icon></span>
+                    <span class="quantity-count padding-vertical-half">1</span>
+                    <span class="quantity-plus padding-vertical-half"><f7-icon f7="plus" class="font-16"></f7-icon></span>
+                </div>
+            </div>
+            <div class="product-note-content text-align-center">
+                <div class="product-note"><Icon name="note" /></div>
+            </div>
+        </div>
+        <div class="product_detail padding display-flex justify-content-space-between align-items-center">
+            <div class="delete-product">
+                <span><f7-icon f7="minus" class="font-16 delete-product-button"></f7-icon></span>
+            </div>
+            <div class="product-summary margin-horizontal-half">
+                <p class="no-margin margin-bottom-half">Margherita Pizza</p>
+                <p class="no-margin"><span>Size: S</span> <span class="text-red">$10.00</span></p>
+            </div>
+            <div class="quantity-section text-align-center padding">
+                <div class="quantity-content display-flex justify-content-center align-items-center">
+                    <span class="quantity-minus padding-vertical-half"><f7-icon f7="minus" class="font-16"></f7-icon></span>
+                    <span class="quantity-count padding-vertical-half">1</span>
+                    <span class="quantity-plus padding-vertical-half"><f7-icon f7="plus" class="font-16"></f7-icon></span>
+                </div>
+            </div>
+            <div class="product-note-content text-align-center">
+                <div class="product-note"><Icon name="note" /></div>
+            </div>
+        </div>
+        <div class="product_detail padding display-flex justify-content-space-between align-items-center">
+            <div class="delete-product">
+                <span><f7-icon f7="minus" class="font-16 delete-product-button"></f7-icon></span>
+            </div>
+            <div class="product-summary margin-horizontal-half">
+                <p class="no-margin margin-bottom-half">Margherita Pizza</p>
+                <p class="no-margin"><span>Size: S</span> <span class="text-red">$10.00</span></p>
+            </div>
+            <div class="quantity-section text-align-center padding">
+                <div class="quantity-content display-flex justify-content-center align-items-center">
+                    <span class="quantity-minus padding-vertical-half"><f7-icon f7="minus" class="font-16"></f7-icon></span>
+                    <span class="quantity-count padding-vertical-half">1</span>
+                    <span class="quantity-plus padding-vertical-half"><f7-icon f7="plus" class="font-16"></f7-icon></span>
+                </div>
+            </div>
+            <div class="product-note-content text-align-center">
+                <div class="product-note"><Icon name="note" /></div>
+            </div>
+        </div>
+        <div class="product_detail padding display-flex justify-content-space-between align-items-center">
+            <div class="delete-product">
+                <span><f7-icon f7="minus" class="font-16 delete-product-button"></f7-icon></span>
+            </div>
+            <div class="product-summary margin-horizontal-half">
+                <p class="no-margin margin-bottom-half">Margherita Pizza</p>
+                <p class="no-margin"><span>Size: S</span> <span class="text-red">$10.00</span></p>
+            </div>
+            <div class="quantity-section text-align-center padding">
+                <div class="quantity-content display-flex justify-content-center align-items-center">
+                    <span class="quantity-minus padding-vertical-half"><f7-icon f7="minus" class="font-16"></f7-icon></span>
+                    <span class="quantity-count padding-vertical-half">1</span>
+                    <span class="quantity-plus padding-vertical-half"><f7-icon f7="plus" class="font-16"></f7-icon></span>
+                </div>
+            </div>
+            <div class="product-note-content text-align-center">
+                <div class="product-note"><Icon name="note" /></div>
+            </div>
+        </div>
+    </div>
+    <!-- ========= CATEGORY POPUP ========= -->
+    <div id="category_popup" class="popup categoryPopup" style="position: fixed; display: block; border-radius: 15px;">
+        <div class="category-form">
+            <div class="text-align-center padding popup_title"></div>
+            <div class="category-add padding">
+                <label class="add_category_name">Category name</label>
+                <div class="margin-top no-margin-bottom display-flex justify-content-center padding-top popup_button">
+                    <button type="button" class="button button-raised text-color-black button-large popup-close margin-right popup-button">Cancel</button>
+                    <button type="button" class="button button-raised button-large popup-button" style="background-color: rgb(243, 62, 62); color: rgb(255, 255, 255);" @click="addCategory">Ok</button>
+                </div>
+            </div>
+            <div><img src="/images/flow.png" style="width:100%"></div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { f7, f7Icon } from 'framework7-vue';
+import Icon from "./Icon.vue"
+</script>
