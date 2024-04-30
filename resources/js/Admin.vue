b91161f707f0a9d43c885f789a27229a65832137
ankur <ankur@intuitionsoftech.com.au>
2024-04-30 18:21:53 +0530

    feedback page finished

--- resources/js/Admin.vue
+++ resources/js/Admin.vue
@@ -5,7 +5,7 @@
         
       </div>
       <f7-view
-        url="/OrdersDetail/"
+        url="/FeedBack/"
         :main="true"
         class="safe-areas"
         :master-detail-breakpoint="768"
