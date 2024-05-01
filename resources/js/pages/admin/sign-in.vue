e2f9274c8269bc39083a64950b9f1e2ce910820a
ankur <ankur@intuitionsoftech.com.au>
2024-05-01 16:26:45 +0530

    sign-in page design start

---
+++ resources/js/pages/admin/sign-in.vue
@@ -0,0 +1,19 @@
+<template>
+    <f7-page>
+        <div class="sign_in_page">
+            <img class="top_left_img" src="\assets\images\seederImages\log_in\top_left_img.png" alt="top-left-img">
+            <img class="left_img" src="\assets\images\seederImages\log_in\left_img.png" alt="left-img">
+            <img class="bottom_right_img" src="\assets\images\seederImages\log_in\bottom_right_img.png" alt="bottom-right-img">
+            <div class="sign_in_outer_wrapper">
+                <div class="sign_in_wrapper"></div>
+            </div>
+        </div>
+    </f7-page>
+</template>
+
+<script setup>
+import {
+    f7Page, f7Navbar, f7BlockTitle, f7Block, f7, f7Breadcrumbs,
+    f7BreadcrumbsItem, f7BreadcrumbsSeparator, f7BreadcrumbsCollapsed,
+} from 'framework7-vue';
+</script>
