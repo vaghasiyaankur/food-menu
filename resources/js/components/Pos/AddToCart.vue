c630c228d97bcc9e29abe5e44c359030cd2ee68f
ankur <ankur@intuitionsoftech.com.au>
2024-05-03 15:33:26 +0530

    table view, product,product section, slider, html css edit design changes

--- resources/js/components/Pos/AddToCart.vue
+++ resources/js/components/Pos/AddToCart.vue
@@ -6,7 +6,8 @@
                 Order
                 {{ table.order ? '#'+table.order.id : '' }}
             </div>
-            <div class="table_number">{{ floorName + ' Table No. ' +  table ?.table_number}}</div>
+            <div class="floor-name">{{floorName}}</div>
+            <div class="table_number">{{ ' Table No. ' +  table ?.table_number}}</div>
         </div>
         <div class="dine-options grid grid-cols-3">
             <button class="button button-small food-received-type" 
--- resources/js/components/Slider.vue
+++ resources/js/components/Slider.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="position-relative category-slider-content">
-        <div data-navigation='{"prevEl":".swiper-button-prev", "nextEl":".swiper-button-next"}' data-space-between="15" data-slides-per-view="4" class="swiper demo-swiper-multiple swiper-init demo-swiper margin-top-half margin-bottom-half">
+    <div class="category-slider-content">
+        <div data-navigation='{"prevEl":".swiper-button-prev", "nextEl":".swiper-button-next"}' data-space-between="15" data-slides-per-view="4" class="swiper demo-swiper-multiple swiper-init demo-swiper">
             <div class="swiper-wrapper">
                 <div class="swiper-slide" 
                     :class="activeCategory === 0 && 'active'"
                     @click="selectCategory(0)"
                     >
-                    <div class="category-image-slider col">
+                    <div class="category-image-slider">
                         <img src="/images/dish.png" alt="">
                     </div>
-                    <p class="slider-category-name margin-left-half text-align-center">All Items</p>
+                    <p class="slider-category-name">All Items</p>
                     <span class="category-item" v-if="activeCategory === 0">{{  productsCount }}</span>
                 </div>
                 <div class="swiper-slide" 
@@ -18,10 +18,10 @@
                     :class="activeCategory === category.id && 'active'"
                     @click="selectCategory(category.id)"
                 >
-                    <div class="category-image-slider col">
+                    <div class="category-image-slider">
                         <img :src="'/storage/' + category.image" :alt="category.name" width="33" height="33"/>
                     </div>
-                    <p class="slider-category-name margin-left-half text-align-center">{{ `${category.name.substr(0, 5)} ${category.name.length > 5 ? '..' : ''}`}}</p>
+                    <p class="slider-category-name">{{ `${category.name.substr(0, 5)} ${category.name.length > 5 ? '..' : ''}`}}</p>
                     <span class="category-item" v-if="activeCategory === category.id">{{ category.product_count }}</span>
                 </div>
             </div>
