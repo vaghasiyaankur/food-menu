fc569d2742c0d1cf319b762168eb3b4cf919211a
ankur <ankur@intuitionsoftech.com.au>
2024-05-04 12:53:31 +0530

    design edit table view, kot view, current orders

--- resources/js/pages/pos/currentOrders.vue
+++ resources/js/pages/pos/currentOrders.vue
@@ -1,13 +1,13 @@
 <template>
     <f7-page>
         <div class="current_orders_page">
-            <div class="card_header current_orders-banner">
-                <div class="kot_view-header_inner">
-                    <div class="kot_view-banner">
-                        <h3 class="no-margin no-padding">KOT View</h3>
+            <div class="card_header">
+                <div class="current_orders-header_inner">
+                    <div class="current_orders-banner">
+                        <h3 class="no-margin no-padding">Current Orders</h3>
                     </div>
-                    <div class="kot_view_navbar">
-                        <div class="kot_view-sorter">
+                    <!-- <div class="kot_view_navbar"> -->
+                        <!-- <div class="kot_view-sorter">
                             <div class="display-flex align-items-center" @click="dineType = 'delivery';getKotList()">
                                 <div class="kot_status-represent-delivery"></div>
                                 <p class="kot_status-represent-type no-margin no-padding">Delivery</p>
@@ -20,14 +20,14 @@
                                 <div class="kot_status-represent-dineIn"></div>
                                 <p class="kot_status-represent-type no-margin no-padding">Dine In</p>
                             </div>
-                        </div>
-                        <div class="kot_view-floor_select">
+                        </div> -->
+                        <div class="current_orders-floor_select">
                             <select class="floor_selector" name="floor_selector" v-model="floor" id="floor_selector" @change="getKotList">
                                 <option value="">Select Floor</option>
                                 <option :value="key" v-for="(floor,key) in floorList" :key="floor">{{ floor }}</option>
                             </select>
                         </div>
-                    </div>
+                    <!-- </div> -->
                 </div>
             </div>
             <div class="card-content current_order_table_list" v-if="paginateData.data?.length > 0">
@@ -66,7 +66,7 @@ const floor = ref('');
 const dineType = ref('');
 
 const getKotList = async () => {
-    const formData = { 'floor' : floor.value, 'dineType' : dineType.value };
+    const formData = { 'floor': floor.value, 'dineType': dineType.value };
     const res = await axios.post("/api/kot-list", formData);
     paginateData.value = res.data
 }
@@ -82,7 +82,7 @@ const getSingleOrder = (id) => {
 }
 
 const serveOrder = async (id, type) => {
-    var fromData = { 'id': id, 'type' : type };
+    var fromData = { 'id': id, 'type': type };
     const res = await axios.post("/api/order-serve", fromData);
     successNotification(res.data.success);
     f7.popup.close(`.current_orders_popup`)
