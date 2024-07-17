8e436f1df9d5d7fed8f473cc0d9d4a2e0538bf5c
meetsardhara33 <meetsardhara33@gmail.com>
2024-07-17 18:40:57 +0530

    super admin panel blade file create

---
+++ resources/views/admin/layout/master.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default"
+    data-assets-path="../assets/" data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>@yield('title', 'Home')</title>
+
+    <meta name="description" content="" />
+
+    @include('admin.layout.style')
+
+    @yield('css')
+</head>
+
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+
+            @include('admin.layout.sidebar')
+
+            <!-- Layout container -->
+            <div class="layout-page">
+                
+                @include('admin.layout.navbar')
+
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    
+                    @yield('content')
+
+                    @include('admin.layout.footer')
+
+                    <div class="content-backdrop fade"></div>
+                </div>
+                <!-- Content wrapper -->
+            </div>
+            <!-- / Layout page -->
+        </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
+
+    @include('admin.layout.script')
+
+    @yield('script')
+    
+</body>
+
+</html>
